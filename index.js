63629bf8a567257b5102a0424ab1cada1a861d6f
TheNaciaaStrike <lukaslsa@gmail.com>
2020-01-30 14:22:42 +0200

    test

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path');
+
+const app = express()
+const port = 3000
+
+let streamTask = [{
+    "taskID" : "1",
+    "user" : "test",
+    "task" : "Zulky",
+    "platofrm": "twich"
+},{
+    "taskID" : "2",
+    "user" : "test",
+    "task" : "Zulky",
+    "platofrm": "twich"
+}]
+
+var taskIDCounter = 1
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.get('/stream', (req, res) =>{
+    res.render('pages/twitch', {streamTask})
+    console.log(streamTask)
+})
+
+app.get('/api/twitch/:user/:task',(req, res) =>{
+    const user = req.params.user
+    const task = req.params.task
+    var addTask = []
+    addTask = {
+        "taskID" : taskIDCounter,
+        "user" : user,
+        "task" : task,
+        "platofrm" : "Twitch"
+    }
+    taskIDCounter ++
+    streamTask.push(addTask)
+    res.send("taskAdded")
+    console.log(streamTask)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
