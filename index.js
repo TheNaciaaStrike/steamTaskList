3604d512c25c2321c4c38e322ac0555116640f31
TheNaciaaStrike <lukaslsa@gmail.com>
2020-03-06 02:01:58 +0200

    some updates

--- index.js
+++ index.js
@@ -1,56 +1,72 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+const Pool  = require('pg') 
+
+const pool = new Pool.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl:true,
+})
+pool.connect()
 
 const app = express()
 const PORT = process.env.PORT || 3000;
 
 let streamTask = []
-
+ 
 var taskIDCounter = 1
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.json()) 
 
 app.get('/', function(req, res) {
     res.render('pages/index');
 })
 
 app.get('/stream', (req, res) =>{
+    console.log(pool.query('SELECT * FROM "stream"."taskList" WHERE "Completed"=false ;'))
     res.sendFile('pages/test.html',{
         root: path.join(__dirname, './views')
     })
     console.log(streamTask)
 })
 
-app.get('/streamJSON', (req, res) =>{
-    res.json(streamTask)
+app.get('/streamJSON',  (req, res) =>{
+    pool.query('SELECT * FROM "stream"."taskList" WHERE "Completed"=false ;', (err, rez) =>{
+        if(err){
+            throw err
+        }
+        console.log(rez.rows)
+        streamTask = rez.rows
+        res.json(streamTask)
+    })
 })
-
 app.get('/api/twitch/:user/:task',(req, res) =>{
-    var user = req.params.user
+    var user = req.params.user  
     var task = req.params.task
-    console.log(task)
     task = task.replace('!givetask ' , '')
-    var addTask = []
-    addTask = {
-        "taskID" : taskIDCounter,
-        "user" : user,
-        "task" : task,
-        "platofrm" : "Twitch"
-    }
-    taskIDCounter ++
-    streamTask.push(addTask)
-    res.send("@" + user + " your task to do " + task + " has been added")
-    console.log(streamTask)
-})
-
-app.get('/deleteTask/:id', (req, res) =>{
-    const deltionID = req.param.id
-    var as = deltionID - 1
-    streamTask.splice(as)
-    res.redirect(301, '../stream')
+    var values = ['Twitch', user , task] 
+    const sql = 'INSERT INTO "stream"."taskList" ("Platform", "RequestedUser", "Task") VALUES ($1, $2, $3);'
+    pool.query(sql, values, (err, rez) =>{
+        if(err){
+            throw  err
+        }
+        res.send("@" + user + " your task to do " + task + " has been added")
+    })
+}) 
+app.get('/markasdone/:id',(req,res) =>{ 
+    var deltionID = req.params.id
+    const sql = 'UPDATE stream."taskList" SET  "Completed"=true WHERE "TaskID"='+deltionID+';'
+    pool.query(sql, (err, rez) =>{
+        if(err){
+            throw err
+        }
+        console.log(err, rez)
+        res.redirect(301, '../')
+    })
+    
 })
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
